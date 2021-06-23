edf0b64f7d046f175cd80efb4c91459ecd5fc30d
José Bruno <60969043+josebruno2020@users.noreply.github.com>
2021-06-23 08:41:08 -0300

    adicionando um interceptor na request

--- src/main.js
+++ src/main.js
@@ -8,6 +8,21 @@ import msg from 'vee-validate/dist/locale/pt_BR';
 import './directives/Transform';
 Vue.use(VueResource);
 Vue.http.options.root = 'http://localhost:3000';
+
+Vue.http.interceptors.push((req, next) => {
+
+    // é possível colocar informações no header antes do envio da requisição
+    // req.headers.set('Authorization', 'informação de segurança aqui');
+    console.log('Lidando com o request');
+
+    next(res => {
+      console.log('Lidando com a resposta')
+      // é possível acessar os dados da reposta e realizar transformações antes
+      console.log(res.body);
+    });
+
+});
+
 Vue.use(VueRouter);
 Vue.use(VeeValidate);
 Validator.localize('pt_BR', msg);
